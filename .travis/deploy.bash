c7848cc6abb13fb92ce630f02a1ab82e1297bad7
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-05-09 01:42:52 +0900

    Fix: add .enc file

--- .travis/deploy.bash
+++ .travis/deploy.bash
@@ -3,9 +3,9 @@ set -eux
 
 # setup ssh-agent and provide the GitHub deploy key
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_3b94903f5871_key -iv $encrypted_3b94903f5871_iv -in id_rsa.enc -out id_rsa -d
-chmod 600 id_rsa
-ssh-add id_rsa
+openssl aes-256-cbc -K $encrypted_3b94903f5871_key -iv $encrypted_3b94903f5871_iv -in .travis/id_rsa.enc -out .travis/id_rsa -d
+chmod 600 .travis/id_rsa
+ssh-add .travis/id_rsa
 
 # commit the assets in docs/ if changed, and push to GitHub using SSH
 git config user.name "${GIT_NAME}"
