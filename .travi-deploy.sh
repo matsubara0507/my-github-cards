5941860c392202033ea62dbcd5bba942a5623291
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-05-09 01:06:01 +0900

    Add TravisCI config

---
+++ .travi-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_3b94903f5871_key -iv $encrypted_3b94903f5871_iv -in id_rsa.enc -out id_rsa -d
+chmod 600 id_rsa
+ssh-add id_rsa
+
+# commit the assets in docs/ if changed, and push to GitHub using SSH
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+git status
+git add docs
+git diff --staged --quiet || git commit -m "[skip ci] Update docs by selfcat"
+git push origin master
